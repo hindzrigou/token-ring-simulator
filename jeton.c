0deaf07425c13abee04de354ac03cf598e4115eb
Hind <zrigouhind@gmail.com>
2025-01-21 17:15:56 +0000

    version final

--- jeton.c
+++ jeton.c
@@ -5,24 +5,24 @@
 #include <string.h>
 int main() {
     int nbr_noeuds= 5; // Nombre de nœuds dans le reseau
-    int premier_jeton= 1; // Jeton initial
+    int jeton= 1; // Jeton initial
     int pipes[5][2];  // Pipes entre les nœuds
-    int token =premier_jeton;  // Jeton initial
+    
     pid_t pid;
-    // Créer les pipes pour chaque nœud
-    for (int i = 0; i < nbr_noeuds; i++) {
-        pipe(pipes[i]); 
-    }
     // Créer les processus pour chaque nœud
     for (int i = 0; i < nbr_noeuds; i++) {
         pid = fork();
 
         if (pid < 0) {
             printf("Erreur lors du creation du fils");
-            
+            // Echoue au cours de la création
         }
+
+        
+
         if(pid == 0) {  // Processus enfant (nœud)
             int jeton_reçus;
+            
             while (1) {
                 // Lire le jeton depuis le pipe d'entrée
                 read(pipes[i][0], &jeton_reçus, sizeof(jeton_reçus));
@@ -34,8 +34,8 @@ int main() {
                 }
 
                 // Passer le jeton au prochain nœud
-                int next_node = (i + 1) % nbr_noeuds;
-                write(pipes[next_node][1], &jeton_reçus, sizeof(jeton_reçus));
+                int noeud_suivant = (i + 1) % nbr_noeuds;
+                write(pipes[noeud_suivant][1], &jeton_reçus, sizeof(jeton_reçus));
                 // Pause pour ralenti l'exécution
                 sleep(1);
             }
@@ -44,7 +44,7 @@ int main() {
     }
     // Processus parent (nœud initial)
     printf("Lancement du réseau Token Ring.\n");
-    write(pipes[0][1], &token, sizeof(token));  // Envoyer le jeton au premier nœud
+    write(pipes[0][1], &jeton, sizeof(jeton));  // Envoyer le jeton au premier nœud
     // Attendre les processus enfants (inactif ici car la simulation est infinie)
     for (int i = 0; i < nbr_noeuds; i++) {
         wait(NULL);
